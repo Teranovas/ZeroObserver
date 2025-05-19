b866fc47f52154c0d3bd12b59083488976b19149
Teranovas <125176569+Teranovas@users.noreply.github.com>
2025-05-19 16:06:59 +0900

    화면설정

---
+++ app/src/main/java/com/example/zeroobserver/WorldMapScreen.kt
@@ -0,0 +1,11 @@
+package com.example.zeroobserver
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WorldMapScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_world_map_screen)
+    }
+}
