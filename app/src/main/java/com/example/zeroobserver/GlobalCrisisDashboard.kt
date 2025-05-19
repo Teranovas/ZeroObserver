60532513efd4b89ecc03a4221a6d90712169f379
Teranovas <125176569+Teranovas@users.noreply.github.com>
2025-05-19 19:08:17 +0900

    화면설정3

---
+++ app/src/main/java/com/example/zeroobserver/GlobalCrisisDashboard.kt
@@ -0,0 +1,11 @@
+package com.example.zeroobserver
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GlobalCrisisDashboard : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_global_crisis_dashboard)
+    }
+}
