5cd154441c8c6544ce03ebe0ebe522ea085d4266
Teranovas <125176569+Teranovas@users.noreply.github.com>
2025-06-30 23:49:14 +0900

    오류해결7

--- app/src/main/java/com/example/zeroobserver/NationProgress.kt
+++ app/src/main/java/com/example/zeroobserver/NationProgress.kt
@@ -1,5 +1,8 @@
 package com.example.zeroobserver
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
 @Entity(tableName = "nation_progress")
 data class NationProgress(
     @PrimaryKey val country: String,
@@ -9,6 +12,3 @@ data class NationProgress(
     val report: String
 )
 
-annotation class PrimaryKey
-
-annotation class Entity(val tableName: String)
